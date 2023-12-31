aad33c4f67973ff541977f0c944b54f63c322627
kkkyaho@naver.com <kkkyaho@naver.com>
2023-12-31 11:50:33 +0900

    init commit

---
+++ src/test/java/com/vuetest/vuebackend/VueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuetest.vuebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,9 @@
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.junit.jupiter.api.Test;
+
+public class test {
+
+    @Test
+    public void test() {
+    }
+}
